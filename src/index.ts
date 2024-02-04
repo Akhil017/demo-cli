c549c7edef6a90f6a85bb4c434217021a4ffe7e9
Akhil017 <akhil.17.akhi@gmail.com>
2024-02-04 18:38:46 +0530

    added commander for cli

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,32 @@
 #!/usr/bin/env node
-const a = 0;
-console.log("aaaaaaaa", a);
+import { Command } from "commander";
+
+// declare the program
+const program = new Command();
+
+// add actions onto the program
+program
+  .argument("<string>", "string to log")
+  .action((message: string) => {
+    console.log("hi " + message);
+  })
+  .description("Say hi");
+
+//  sub commands
+program
+  .command("add <numbers...>")
+  .action((numbers: Array<number>) => {
+    const parsedNumbers = numbers.map(Number);
+    const total = parsedNumbers.reduce((a, b) => a + b, 0);
+    console.log("total", total);
+  })
+  .description("Add numbers and log the total");
+
+program.command("max <numbers...>").action((numbers: Array<number>) => {
+  const parsedNumbers = numbers.map(Number);
+  const max = Math.max(...parsedNumbers);
+  console.log("Max is : ", max);
+});
+
+// execute the cli with the given arguments
+program.parse(process.argv);
